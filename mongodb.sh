ad2090d0970edf1b9390ec383194d746007d3e84
chinnulakku <chinnulakku4@gmail.com>
2025-05-28 15:39:03 +0530

    roboshop

---
+++ mongodb.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+ID=$(id -u)
+R="\e[31m"]
+G="\e[32m"]
+Y="\e[33m"]
+N="\e[0m"]
+
+TIMESTAMP=$(date +%F-%H-%M-%S)
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
+
+echo "script started executing at $TIMESTAMP" &>> $LOGFILE
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+       echo -e "$2 ...$R FAILED $N"
+    else
+        echo -e "$2 ... $G SUCCESS $N"
+    fi
+}
+
+if [ $ID -ne 0 ]
+then
+    echo -e "$R ERROR:: Please run this script with root access $N"
+    exit 1 # you can give other than 0
+else
+    echo "you are root user"
+fi # fi means reverse of if, indicating condition end
+
+cp mongo.repo /etc/yum.repo.d/mongo.repo &>> $LOGFILE
+
+VALIDATE $? "copied MongoDB Repo"
+
+dnf install mongodb-org -y &>> $LOGFILE
+
+VALIDATE $? " Installing mongodb"
+
+systemctl enable mongodb  &>> $LOGFILE
+
+validate $? "Enabling mongodb"
+
+systemctl start mongodb  &>> $LOGFILE
+
+validate $? "Starting mongodb"
+
+
+
+
+systemctl restrt mongodb
+
+validate $? "Restarting mongodb"
