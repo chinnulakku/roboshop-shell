50421b9c92cf3606e9f65dd1ab9738894c30a9fb
chinnulakku <chinnulakku4@gmail.com>
2025-05-29 13:35:32 +0530

    roboshop

--- redis.sh
+++ redis.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 ID=$(id -u)
-R="\e[31m"]
-G="\e[32m"]
-Y="\e[33m"]
-N="\e[0m"]
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
 
-TIMESTAMP=$[date +%F-%H-%M-%S]
+TIMESTAMP=$(date +%F-%H-%M-%S)
 LOGFILE="/etc/$0-$TIMESTAMP.log"
 
 echo "script stated executing at $TIMESTAMP" &>> $LOGFILE
