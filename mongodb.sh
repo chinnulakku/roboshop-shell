ab7584f42052fa523c7675292ca62b4171092464
chinnulakku <chinnulakku4@gmail.com>
2025-05-29 06:01:50 +0530

    roboshop

--- mongodb.sh
+++ mongodb.sh
@@ -44,7 +44,7 @@ systemctl start mongod &>> $LOGFILE
 
 VALIDATE $? "Starting MongoD"
 
-sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.config &>> $LOGFILE
+sed -e 's/127.0.0.1/0.0.0.0/g' /etc/mongod.config &>> $LOGFILE
 
 VALIDATE $? " Remote access to MongoDB"
 
