0c7720adb216d2a96f79da56e287dcc9f7da1adb
chinnulakku <chinnulakku4@gmail.com>
2025-05-30 13:07:50 +0530

    roboshop

--- shipping.sh
+++ shipping.sh
@@ -84,10 +84,6 @@ dnf install mysql -y &>> $LOGFILE
 
 VALIDATE $? "install MySQL client"
 
-mysql -h mysql.sudhaaru676.online -uroot -pRoboShop@1 < /app/schema/shipping.sql 
-
-VALIDATE $? "loading shipping data"
-
 systemctl restart shipping &>> $LOGFILE
 
 VALIDATE $? "restart shipping"
