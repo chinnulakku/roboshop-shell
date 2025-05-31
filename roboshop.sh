4bcdbab0b25769dd82129dc2b08164b097e7c640
chinnulakku <chinnulakku4@gmail.com>
2025-05-31 14:06:13 +0530

    roboshop

--- roboshop.sh
+++ roboshop.sh
@@ -3,11 +3,10 @@
 AMI=ami-0b4f379183e5706b9
 SG_ID=sg-061ecf2f6de064124 #replace with your SG ID
 INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "web")
-ZONE_ID=Z003779318P2IJXTXT1UI # replace your zone ID
-DOMAIN_NAME="sudhaaru676.online"
 
 for i in "${INSTANCES[@]}"
 do
+    echo "instance is: $i"
     if [ $i == "mongodb" ] || [$i == "mysql"] || [$i == "shipping"]
     then
         INSTANCE_TYPE="t3.small"
@@ -15,26 +14,5 @@ do
         INSTANCE_TYPE="t2.micro"
     fi
 
-    IP_ADDRESS=$(aws ec2 run-instances --image-id ami-0b4f379183e5706b9 -- instance-type $INSTANCE_TYPE --security-group-ids sg-061ecf2f6de064124 --tag-specifications "ResourceType=instance, Tags=[{Key=Name,Value=$i}]" --query 'Instances[0].PrivateIpAddress' --output text)
-    echo "$i:$IP_ADDRESS"
-
-    #create R53 record, make sure yu delete existing record
-    aws route53 change-resource-record-sets \
-    --hosted-zone-id $ZONE_ID \
-    --change-batch '
-    {
-        "Comment": "Creating a record set for cognito endpoint"
-        ,"Changes": [{
-        "Action"                : "CREATE"
-        ,"ResourceRecordset"    : {
-            "Name"              : "'$i'. '$DOMAIN_NAME'"
-            ,"Type"             : "A"
-            ,"TTL"              : 1
-            ,"ResourceRecords"  : [{
-                "value"         : "'$IP_ADDRESS'"
-            }]
-        }
-        }]
-    }
-        '
-    done
+    aws ec2 run-instances --image-id ami-0b4f379183e5706b9 -- instance-type $INSTANCE_TYPE --security-group-ids sg-061ecf2f6de064124 --tag-specifications "ResourceType=instance, Tags=[{Key=Name,Value=$i}]" 
+done
