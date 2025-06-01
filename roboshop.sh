68630240fc57c3f2434d7b5dc9730faa52567f8d
chinnulakku <chinnulakku4@gmail.com>
2025-06-01 17:59:51 +0530

    roboshop

--- roboshop.sh
+++ roboshop.sh
@@ -6,13 +6,13 @@ INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipp
 
 for i in "${INSTANCES[@]}"
 do
-    echo "instance is: $i"
-    if [ $i == "mongodb"] || [ $i == "mysql" ] || [ $i == "shipping" ]
+    echo "instance is:$i"
+    if [ $i == "mongodb" ] || [ $i == "mysql" ] || [ $i == "shipping" ]
     then
         INSTANCE_TYPE="t3.small"
     else
         INSTANCE_TYPE="t2.micro"
     fi
 
-aws ec2 run-instances --image-id ami-0b4f379183e5706b9 --instance-type $INSTANCE_TYPE --security-group-ids sg-061ecf2f6de064124
+aws ec2 run-instances --image-id ami-0b4f379183e5706b9 --instance-type $INSTANCE_TYPE --security-group-ids sg-061ecf2f6de064124 --tag-specifications "ResourceType=instance, Tags=[{key=name,value=$i}]"
 done
