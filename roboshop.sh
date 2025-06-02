b23051de11c9bc7e42870a86e9515dd4d4c082ca
chinnulakku <chinnulakku4@gmail.com>
2025-06-02 14:45:04 +0530

    roboshop

--- roboshop.sh
+++ roboshop.sh
@@ -21,7 +21,7 @@ do
     # Corrected: Create R53 record, make sure to delete existing record
     aws route53 change-resource-record-sets \
     --hosted-zone-id $ZONE_ID \
-    --change batch '
+    --change-batch '
     {
         "Comment": "Creating a record set for cognito endpoint"
         ,"Changes": [{
