3c42d0cd41094cd7c1dd994c1ab1a20412833485
chinnulakku <chinnulakku4@gmail.com>
2025-06-02 14:00:46 +0530

    roboshop

--- roboshop.sh
+++ roboshop.sh
@@ -24,7 +24,7 @@ do
 aws route53 change-resource-record-sets \
 --hosted-zone-id "$ZONE_ID" \
 --change-batch 
-    {
+{
     "Comment": "Creating a record set for cognito endpoint",
     "Changes": [{
         "Action": "UPSERT",
