036b515d12dfe2d9d8d08d712a64eb7d4b01d641
chinnulakku <chinnulakku4@gmail.com>
2025-06-02 13:32:26 +0530

    roboshop

--- roboshop.sh
+++ roboshop.sh
@@ -18,13 +18,15 @@ do
     IP_ADDRESS=$(aws ec2 run-instances --image-id ami-0b4f379183e5706b9 --instance-type $INSTANCE_TYPE --security-group-ids sg-061ecf2f6de064124 --tag-specifications "ResourceType=instance, Tags=[{Key=Name,Value=$i}]" --query 'Instances[0].PrivateIpAddress' --output text)
     echo "$i: $IP_ADDRESS"
 
-    #create R53 record, make sure yo delete existing record
-    aws route53 change-resource-record-sets \
-    --hosted-zone-id $ZONE_ID \
-    --change-batch '
-    {
-        "Comment": "Creating a record set for cognito endpoint",
-        "Changes": [{
+    # Corrected: Create R53 record, make sure to delete existing record
+# This assumes $ZONE_ID, $i, $DOMAIN_NAME, and $IP_ADDRESS are defined earlier in your script.
+
+aws route53 change-resource-record-sets \
+--hosted-zone-id "$ZONE_ID" \
+--change-batch  <<EOF
+{
+    "Comment": "Creating a record set for cognito endpoint",
+    "Changes": [{
         "Action": "UPSERT",
         "ResourceRecordSet": {
             "Name": "${i}.${DOMAIN_NAME}",
