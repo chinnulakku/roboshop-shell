3725c9cdf1603c57170d8c7295f1e2c5fcb6841f
chinnulakku <chinnulakku4@gmail.com>
2025-06-02 14:40:56 +0530

    roboshop

--- roboshop.sh
+++ roboshop.sh
@@ -4,7 +4,7 @@ AMI=ami-0b4f379183e5706b9
 SG_ID=sg-061ecf2f6de064124 #replace with your SG ID
 INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "web")
 ZONE_ID=Z003779318P2IJXTXT1UI #replace your zone ID
-DOMAIN_NAME="sudhaaru676.online"
+DOMAIN_NAME=sudhaaru676.online
 
 for i in "${INSTANCES[@]}"
 do
